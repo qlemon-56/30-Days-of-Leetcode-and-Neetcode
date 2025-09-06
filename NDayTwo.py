3ae1fb3785f92ebd5f59c3522015cb4fc366765e
Mark <75143899+qlemon-56@users.noreply.github.com>
2025-09-06 23:13:50 +0100

    Create NDayTwo.py

---
+++ NDayTwo.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in enumerate(nums):
+            for x in enumerate(nums):
+                if i[0] == x[0]:
+
+                    continue
+
+                if i[1]+x[1] == target:
+                    
+                    return [i[0], x[0]]
+
+
+# initial solution
+
+# problem name = two_sum
+# what i learnt
+#.index() array method return the position of the first occurence of the argument
+# using enumerate() is better in for loops because it keeps note of the index and the value at that index
