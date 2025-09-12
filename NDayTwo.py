58ebc05b2906b48b2b019b67403513f492abb489
Mark <75143899+qlemon-56@users.noreply.github.com>
2025-09-12 20:41:04 +0100

    Update NDayTwo.py

--- NDayTwo.py
+++ NDayTwo.py
@@ -1,6 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        indices = {}
+            
+        for n, m in enumerate(nums):
+            
+            indices[m] = n
+
+        for i, j in enumerate(nums):
+
+            if target - j in indices.keys():
+                if i != indices[target-j]:
+                    return [i , indices[target-j]]
+                    
+# final solution, O(N) complexity
+                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         for i in enumerate(nums):
             for x in enumerate(nums):
                 if i[0] == x[0]:
@@ -18,3 +35,4 @@ class Solution:
 # what i learnt
 #.index() array method return the position of the first occurence of the argument
 # using enumerate() is better in for loops because it keeps note of the index and the value at that index
+# i leart how to use .keys() and .values() methods for hashmaps
